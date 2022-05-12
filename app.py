1e9e969fb4a802534b40014e387d65feacdf94a0
myankit <ankitdd25@gmail.com>
2022-05-12 16:59:49 +0530

    this is my first commit

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-
 app = Flask(__name__)
-
-@app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
@@ -79,5 +76,4 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
